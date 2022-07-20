c40242d7a0a87d30ae960580ab66aeae903772e7
viniciusroratto <viniciusroratto@gmail.com>
2022-07-20 11:06:33 -0300

    targets speed

--- run.bash
+++ run.bash
@@ -1,10 +1,10 @@
 source ./devel/setup.bash
 gnome-terminal --tab -e "roslaunch tcc_gazebo tcc.launch" 
-sleep 2
+sleep 5
 #gnome-terminal --tab -e "rosrun tcc_pack talker.py"
 #gnome-terminal --tab -e "rosrun tcc_pack listener.py"
 gnome-terminal --tab -e "rosrun tcc_pack move.py"
-sleep 120
+sleep 60
 rosnode kill -a
 pkill roslaunch
 #rosservice call /gazebo/reset_world
--- src/tcc_pack/scripts/move.py
+++ src/tcc_pack/scripts/move.py
@@ -5,7 +5,7 @@ from gazebo_msgs.srv import SetModelState
 from gazebo_msgs.srv import GetModelState
 from std_msgs.msg import String
 from random import seed
-from random import randint
+from random import randint, uniform
 
 import time
 
@@ -20,6 +20,10 @@ uavs = list(filter(lambda k: 'UAV' in k, models))
 targets = list(filter(lambda k: 'target' in k, models))
 boxes = list(filter(lambda k: 'box' in k, models))
 world_size = 50
+target_max_speed = 1
+target_z = 0
+uav_max_speed = 20
+uav_z = 3
 
 
 def move(name, x,y,z):
@@ -51,7 +55,7 @@ def acelerate_to(name, speed_x, speed_y, speed_z):
     x = model_coordinates("UAV_00", "").pose.position.x
     y = model_coordinates("UAV_00", "").pose.position.y
     z = model_coordinates("UAV_00", "").pose.position.z
-    move(name, x + speed_x , y + speed, x + speed_z)
+    move(name, x + speed_x , y + speed_y, z + speed_z)
     	
 
 
@@ -60,10 +64,14 @@ def callback(data):
 
 def reset(blocks = 0):
     
+   
     for each in boxes:
-        x = randint(-world_size, world_size)
-        y = randint(-world_size, world_size)
-        move(each, x+blocks*world_size, y+blocks*world_size, 0)   
+        if blocks == 1:
+            x = randint(-world_size, world_size)
+            y = randint(-world_size, world_size)
+            move(each, x, y, 0)
+        else:
+            move(each, world_size+1, world_size+1, 0)
         
         
     for each in uavs:
@@ -71,17 +79,38 @@ def reset(blocks = 0):
         y = randint(-world_size, world_size)
         move(each, x, y, 3)
       
-      for each in targets:
-          x = randint(-world_size, world_size)
-          y = randint(-world_size, world_size)
-          move(each, x, y, 0)
+    for each in targets:
+        x = randint(-world_size, world_size)
+        y = randint(-world_size, world_size)
+        move(each, x, y, 0)
 
+def targets_movement(targets):
+
+	model_coordinates = rospy.ServiceProxy( '/gazebo/get_model_state', GetModelState)
+	
+	while True:
+		time.sleep(2)
+		for each in targets:
+			x = uniform(-target_max_speed, target_max_speed)
+			y = uniform(-target_max_speed, target_max_speed)
+        	
+			if(model_coordinates(each, "").pose.position.x <= world_size) and (model_coordinates(each, "").pose.position.y <= world_size):
+				acelerate_to(each, x, y, target_z)
+			else:
+				if (model_coordinates(each, "").pose.position.x >= world_size):
+					acelerate_to(each, -x, y, target_z)
+				else:
+					acelerate_to(each, x, -y, target_z)
+	
 
 def main():
 
-    while True:
-    	time.sleep(3)
-        reset(0)
+	reset(0)
+	targets_movement(targets)
+
+	
+		
+
 
     #while True:
      #   time.sleep(1)
