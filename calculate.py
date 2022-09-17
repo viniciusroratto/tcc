6ecf9bf40d0b4e5f8341bede6b4ef34727d4f7cd
viniciusroratto <viniciusroratto@gmail.com>
2022-09-17 23:22:29 +0200

    saturday

--- calculate.py
+++ calculate.py
@@ -38,23 +38,44 @@ def calc_values(paths):
 	dfs = []
 	for each in paths:
 		dfs.append(pd.read_csv(each, header = None))
-		
 	final_df = pd.concat(dfs)
-	#print(final_df.head())
 	
-	big_list = []
-	for column in final_df:
-		big_list.append(mean(list(final_df[column])))
+	header = []
+	for index, content in enumerate(range(len(final_df.columns)-5)):
+		header.append('target_' + str(index))
+		
+	header = header + ['time', 'algo', 'handover', 'prediction', 'variable']
+	final_df.columns = header
+
+	return final_df
 	
-	avg = mean(big_list)
-	
-	std = stdev(big_list)
-	err = 1.96 * std / math.sqrt(len(big_list))/avg
-	
-	#print(avg, std, err)
-	
-	return [avg, std, err]
-	
+def save_values(df, handover, prediction, variable, test_name):
+	filtered = df[(df['handover'] == handover) & (df['prediction'] == prediction) & (df['variable'] == variable)]
+		
+	algo_list = []
+	for each in filtered['algo'].unique():
+		algo_filtered = filtered[filtered['algo'] == each]
+		row_averages = []
+		for index, row in algo_filtered.iterrows():
+			flat_list = row.values.flatten()
+			row_average = mean(flat_list[:-5])
+			row_averages.append(row_average)
+		algo_mean = mean(row_averages)
+		try:
+			algo_std = stdev(row_averages)
+		except: 
+			algo_std = 0
+			
+		try:	
+			algo_error = 1.96 * algo_std / math.sqrt(len(row_averages))/algo_mean
+		except:
+			algo_error = 0
+		results_list = [test_name, each, len(row_averages), "{:.2f}".format(algo_mean), "{:.2f}".format(algo_std), "{:.2f}".format(algo_error)]
+		algo_list.append(results_list)
+	return(algo_list)
+
+def flatten(l):
+    return [item for sublist in l for item in sublist]
 
 directory = './results'
 dirs = [x[0] for x in os.walk(directory)]
@@ -86,16 +107,41 @@ for each in dirs:
 		path = each + '/' + files
 		time_paths.append(path)
 	
+	print(visit_paths, time_paths)
 	#print('visit')
 	visit_values = calc_values(visit_paths)
 	#print('time')
 	time_values = calc_values(time_paths)
 	
-	df = df.append({'title': each, 'avg_visits' : visit_values[0], 'avg_time' : time_values[0], 
-	'std_visit' : visit_values[1], 'std_time':time_values[1], 'err_visits':visit_values[2], 'err_time':time_values[2]}, ignore_index = True)
-print(df)	
+	#print(test_name, each, len(row_averages), "{:.2f}".format(algo_mean), "{:.2f}".format(algo_std), "{:.2f}".format(algo_error))
+	header = ['Test Name', 'Algo', 'Sample Size', 'Average', 'Stdev', '% Err']
+	value_list = []
 	
-df.to_csv('./final_results.csv')
+	value_list.append(save_values(visit_values, False, False, False, 'visit-1'))
+	value_list.append(save_values(time_values, False, False, False, 'time-1'))
+	
+	value_list.append(save_values(visit_values, False, True, False, 'visit-2'))
+	value_list.append(save_values(time_values, False, True, False, 'time-2'))
+	
+	value_list.append(save_values(visit_values, True, False, False, 'visit-3'))
+	value_list.append(save_values(time_values, True, False, False, 'time-3'))
+	
+	value_list.append(save_values(visit_values, True, True, False, 'visit-4'))
+	value_list.append(save_values(time_values, True, True, False, 'time-4'))
+	
+	value_list.append(save_values(visit_values, True, False, True, 'visit-5'))
+	value_list.append(save_values(time_values, True, False, True, 'time-5'))
+	
+	value_list.append(save_values(visit_values, True, True, True, 'visit-6'))
+	value_list.append(save_values(time_values, True, True, True, 'time-6'))
+	
+	
+	df = pd.DataFrame(flatten(value_list), columns = header)
+	print(df)
+	df.to_csv('./final_results.csv')
+	
+	
+	
 	
 	
 
